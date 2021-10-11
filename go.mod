074d29afd3f4c4b51af1f7cb9a35b0c72b35ad00
florindragos <florin@aserto.com>
2021-10-11 09:47:36 +0000

    Implement delete

--- go.mod
+++ go.mod
@@ -3,11 +3,10 @@ module github.com/aserto-dev/aserto-idp-plugin-json
 go 1.16
 
 require (
-	github.com/aserto-dev/go-grpc v0.0.6
-	github.com/aserto-dev/idp-plugin-sdk v0.0.1
-	github.com/aserto-dev/mage-loot v0.4.11
+	github.com/aserto-dev/go-grpc v0.0.10
+	github.com/aserto-dev/idp-plugin-sdk v0.0.2
+	github.com/aserto-dev/mage-loot v0.4.12
 	github.com/magefile/mage v1.11.0
 	golang.org/x/sys v0.0.0-20210930141918-969570ce7c6c
 	google.golang.org/grpc v1.41.0
-	google.golang.org/protobuf v1.27.1
 )
--- pkg/srv/srv.go
+++ pkg/srv/srv.go
@@ -1,34 +1,19 @@
 package srv
 
 import (
-	"bufio"
-	"bytes"
 	"encoding/json"
 	"errors"
 	"io"
-	"log"
-	"os"
+	"io/ioutil"
 
 	api "github.com/aserto-dev/go-grpc/aserto/api/v1"
-	"github.com/aserto-dev/idp-plugin-sdk/pb"
 	"github.com/aserto-dev/idp-plugin-sdk/plugin"
-	"google.golang.org/protobuf/encoding/protojson"
 )
 
-var jsonOptions = protojson.MarshalOptions{
-	Multiline:       false,
-	Indent:          "  ",
-	AllowPartial:    true,
-	UseProtoNames:   true,
-	UseEnumNumbers:  false,
-	EmitUnpopulated: false,
-}
-
 type JsonPlugin struct {
-	Config  *JsonPluginConfig
-	decoder *json.Decoder
-	users   bytes.Buffer
-	count   int
+	Config      *JsonPluginConfig
+	op          plugin.OperationType
+	jsonContent []map[string]interface{}
 }
 
 func NewJsonPlugin() *JsonPlugin {
@@ -41,80 +26,94 @@ func (s *JsonPlugin) GetConfig() plugin.PluginConfig {
 	return &JsonPluginConfig{}
 }
 
-func (s *JsonPlugin) Open(cfg plugin.PluginConfig) error {
+func (s *JsonPlugin) Open(cfg plugin.PluginConfig, operation plugin.OperationType) error {
 	config, ok := cfg.(*JsonPluginConfig)
 	if !ok {
 		return errors.New("invalid config")
 	}
 	s.Config = config
-	s.count = 0
+	s.op = operation
+	switch operation {
+	case plugin.OperationTypeWrite:
+		{
+			s.jsonContent = make([]map[string]interface{}, 0)
+		}
+	case plugin.OperationTypeRead, plugin.OperationTypeDelete:
+		{
+			file, err := ioutil.ReadFile(s.Config.File)
+			if err != nil {
+				return err
+			}
+			err = json.Unmarshal(file, &s.jsonContent)
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
 
 func (s *JsonPlugin) Read() ([]*api.User, error) {
-	if s.decoder == nil {
-		r, err := os.Open(s.Config.File)
-		if err != nil {
-			return nil, err
-		}
-
-		s.decoder = json.NewDecoder(r)
-
-		if _, err = s.decoder.Token(); err != nil {
-			log.Println(err)
-			return nil, err
-		}
-	}
-
-	if s.decoder.More() {
-		u := api.User{}
-		if err := pb.UnmarshalNext(s.decoder, &u); err != nil {
-			return nil, err
-		}
-
-		return []*api.User{&u}, nil
-	} else {
-		if _, err := s.decoder.Token(); err != nil {
-			return nil, err
-		}
-
+	var user api.User
+	if len(s.jsonContent) == 0 {
 		return nil, io.EOF
 	}
+
+	u, err := json.Marshal(s.jsonContent[0])
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(u, &user)
+	if err != nil {
+		return nil, err
+	}
+
+	s.jsonContent = s.jsonContent[1:]
+
+	return []*api.User{&user}, nil
 }
 
 func (s *JsonPlugin) Write(user *api.User) error {
-	if s.count == 0 {
-		s.users.Write([]byte("[\n"))
-	} else {
-		_, _ = s.users.Write([]byte(",\n"))
-	}
-	b, err := jsonOptions.Marshal(user)
+	var userInterface map[string]interface{}
+	u, err := json.Marshal(user)
 	if err != nil {
 		return err
 	}
 
-	if _, err := s.users.Write(b); err != nil {
+	err = json.Unmarshal(u, &userInterface)
+	if err != nil {
 		return err
 	}
-	s.count++
+
+	s.jsonContent = append(s.jsonContent, userInterface)
 
 	return nil
 }
 
+func (s *JsonPlugin) Delete(userId string) error {
+	for i := len(s.jsonContent) - 1; i >= 0; i-- {
+		if s.jsonContent[i]["id"].(string) == userId {
+			s.jsonContent = append(s.jsonContent[:i], s.jsonContent[i+1:]...)
+		}
+	}
+	return nil
+}
+
 func (s *JsonPlugin) Close() error {
-	_, err := s.users.Write([]byte("\n]\n"))
-	if err != nil {
-		return err
+	switch s.op {
+	case plugin.OperationTypeWrite, plugin.OperationTypeDelete:
+		{
+			fileContent, err := json.Marshal(s.jsonContent)
+			if err != nil {
+				return err
+			}
+			err = ioutil.WriteFile(s.Config.File, fileContent, 0644)
+			if err != nil {
+				return err
+			}
+		}
 	}
-	f, err := os.Create(s.Config.File)
-	if err != nil {
-		return err
-	}
-	w := bufio.NewWriter(f)
-	_, err = s.users.WriteTo(w)
-	if err != nil {
-		return err
-	}
-	w.Flush()
+
 	return nil
 }
