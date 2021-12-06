883eeb35bb2441a0c44eeb70b3944b7c2ae34258
oanatmaria <oana@aserto.com>
2021-12-06 12:40:24 +0200

    Push binaries to Github registry

--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.16
 require (
 	github.com/aserto-dev/go-grpc v0.0.10
 	github.com/aserto-dev/idp-plugin-sdk v0.0.2
-	github.com/aserto-dev/mage-loot v0.4.12
+	github.com/aserto-dev/mage-loot v0.4.16
+	github.com/aserto-dev/sver v1.3.9
 	github.com/hashicorp/go-multierror v1.0.0
 	github.com/magefile/mage v1.11.0
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/sys v0.0.0-20210930141918-969570ce7c6c
-	google.golang.org/grpc v1.41.0
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881
+	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
 )
--- magefile.go
+++ magefile.go
@@ -3,12 +3,16 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"path/filepath"
 	"runtime"
 
 	"github.com/aserto-dev/mage-loot/buf"
 	"github.com/aserto-dev/mage-loot/common"
 	"github.com/aserto-dev/mage-loot/deps"
+	"github.com/aserto-dev/sver/pkg/sver"
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
@@ -19,6 +23,19 @@ func init() {
 	// Set private repositories
 	os.Setenv("GOPRIVATE", "github.com/aserto-dev")
 }
+
+var (
+	oras         = deps.BinDep("oras")
+	mediaType    = ":application/vnd.unknown.layer.v1+txt"
+	pluginName   = "aserto-idp-plugin-json_"
+	execLocation = filepath.Join("dist", pluginName)
+	ghName       = "ghcr.io/aserto-dev/" + pluginName
+	osMap        = map[string][]string{
+		"linux":   {"arm64", "amd64"},
+		"darwin":  {"arm64", "amd64"},
+		"windows": {"amd64"},
+	}
+)
 
 // Generate generates all code.
 func Generate() error {
@@ -78,4 +95,34 @@ func All() error {
 
 func Run() error {
 	return sh.RunV("./bin/" + runtime.GOOS + "-" + runtime.GOARCH + "/aserto-idp")
+}
+
+func Publish() error {
+
+	username := os.Getenv("DOCKER_USERNAME")
+	if username == "" {
+		return errors.New("env var DOCKER_USERNAME is not set")
+	}
+	password := os.Getenv("DOCKER_PASSWORD")
+	if password == "" {
+		return errors.New("env var DOCKER_PASSWORD is not set")
+	}
+
+	version, err := sver.CurrentVersion(true, true)
+	if err != nil {
+		return fmt.Errorf("couldn't calculate current version: %w", err)
+	}
+
+	for os, archs := range osMap {
+		for _, arch := range archs {
+			grName := fmt.Sprintf("%s%s_%s:%s", ghName, os, arch, version)
+			location := fmt.Sprintf("%s%s_%s%s", execLocation, os, arch, mediaType)
+			err = oras("push", "-u", username, "-p", password, grName, location)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
